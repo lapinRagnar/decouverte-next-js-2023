2980fa20429a3c4d1ddacf26e99d384cce2d7c2d
lapinRagnar <lapinragnar@gmail.com>
2023-10-16 22:22:19 +0200

    creation d'un ticket create form ajout de lien pour creer le ticket CRUD - Create

--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,19 @@ export default function Home() {
     <main className="flex flex-col ">
 
       <h2>Dashboard</h2>
+      
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
-      <div className="flex justify-center my-8">
+      <div className="flex justify-center my-8 gap-7">
+        
         <Link href="/tickets">
           <button className="btn-primary">View Tickets</button>
         </Link>
+
+        <Link href="/tickets/create">
+          <button className="btn-primary">Create Tickets</button>
+        </Link>
+
       </div>
 
       <h2>Company Updates</h2>
@@ -20,6 +27,7 @@ export default function Home() {
         <h3>New member of the web dev team...</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti.</p>
       </div>
+
       <div className="card">
         <h3>New website live!</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, at quam. Dolores omnis possimus quam soluta rerum illo laborum ullam pariatur molestiae, modi beatae corrupti, assumenda distinctio adipisci, cupiditate minima eum vitae? Similique dicta est facilis debitis, autem temporibus quo repellat illum unde id iste veritatis eveniet, aspernatur enim quas.</p>
